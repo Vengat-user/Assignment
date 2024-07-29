d67b0f2adbb4448761873f95cc72cbef4c1a8988
vengatesh <vengatbharat@gmail.com>
2024-07-29 15:12:16 +0530

    Docker_image

--- docker/main.py
+++ docker/main.py
@@ -1,8 +1,16 @@
-
 from fastapi import FastAPI
+from settings import settings
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+def read_root():
+    return {"message": "Welcome to the FastAPI application!"}
+
+@app.get("/settings/{key}")
+def read_setting(key: str):
+    value = settings.get_attribute(key)
+    if value:
+        return {key: value}
+    else:
+        return {"error": "Key not found"}
---
+++ practice.py
@@ -0,0 +1,12 @@
+
+#
+# #ef is_anag(s1:str,s2:str):
+
+#   if(s1.index == s2.index):
+# #        print("true")
+# #    else:
+
+#       print("false")
+# #is_anag("listen","list")
+
+#s_anag("triangle","integral")
